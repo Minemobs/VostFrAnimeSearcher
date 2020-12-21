3927f39c1fb27daa71ac9bc010d7eab1b48c5449
minemobs <issahakim94@gmail.com>
2020-12-21 18:34:15 +0100

    I've forogot to delete one func

--- src/main/java/fr/minemobs/animes/AnimeSearcherAPI.java
+++ src/main/java/fr/minemobs/animes/AnimeSearcherAPI.java
@@ -39,22 +39,4 @@ public class AnimeSearcherAPI {
         }
         return null;
     }
-
-    private static String readUrl(String urlString) throws Exception {
-        BufferedReader reader = null;
-        try {
-            URL url = new URL(urlString);
-            reader = new BufferedReader(new InputStreamReader(url.openStream()));
-            StringBuffer buffer = new StringBuffer();
-            int read;
-            char[] chars = new char[1024];
-            while ((read = reader.read(chars)) != -1)
-                buffer.append(chars, 0, read);
-
-            return buffer.toString();
-        } finally {
-            if (reader != null)
-                reader.close();
-        }
-    }
 }
